a3b8749cb3dedafad10d4d3ebc44624942908fea
jmptable <trueowen@verizon.net>
2012-08-15 10:36:36 -0700

    Revert "Fixed check for PORT env var so that it is chosen before my port."
    This reverts commit 7532f44e7e71c1af20b9144dda4d797c3c514c67.
    
    Conflicts:
    
    	.shared-history.js.swp
    	shared-history.js

--- shared-history.js
+++ shared-history.js
@@ -103,6 +103,10 @@ function makeID(socket) {
 var server = net.createServer(newSocket);
  
 //listen on port 8888
+<<<<<<< HEAD
 var port = process.env.PORT || 8888;
 server.listen(port);
 console.log("Listening on port "+port);
+=======
+server.listen(8888 || process.env.PORT);
+>>>>>>> parent of 7532f44... Fixed check for PORT env var so that it is chosen before my port.
